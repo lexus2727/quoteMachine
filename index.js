f4833dbb87a7c07096993fc9143d9f6665345712
lexus2727 <lexus2424@yahoo.com>
2020-09-03 19:44:25 -0400

    js set up

---
+++ index.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+let quote = [
+    ‘“The nature of yoga is to shine the light of awareness into the darkest corners of the body.”
+    -Jason Crandell’,
+    ‘“Change only happens in the present moment. The past is already done. The future is just
+    energy and intention.” -Kino MacGregor’,
+    ‘“Yoga begins with listening. When we listen, we are giving space to what is.” -Richard
+    Freeman’,
+    ‘“The body benefits from movement, and the mind benefits from stillness.” -Sakyong Mipham’,
+    ‘“Yoga does not transform the way we see things, it transforms the person who sees it.” -B.K.S
+    Iyengar’,
+    ‘“Happiness does not depend on what you have or who you are. It solely relies on what you
+    think.” -Buddha’,
+    ‘“In the middle of difficulty lies opportunity.” -Albert Einstein’
+];
