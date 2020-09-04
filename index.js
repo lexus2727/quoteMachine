01c4f6d103695d6713f8a1323023f7f18f8f9f30
lexus2727 <lexus2424@yahoo.com>
2020-09-03 20:26:27 -0400

    quote machine complete

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 let btn = document.getElementById('btn');
 let output = document.getElementById('output');
 let quote = [
-    ‘“The nature of yoga is to shine the light of awareness into the darkest corners of the body.”
-    -Jason Crandell’,
-    ‘“Change only happens in the present moment. The past is already done. The future is just
-    energy and intention.” -Kino MacGregor’,
-    ‘“Yoga begins with listening. When we listen, we are giving space to what is.” -Richard
-    Freeman’,
-    ‘“The body benefits from movement, and the mind benefits from stillness.” -Sakyong Mipham’,
-    ‘“Yoga does not transform the way we see things, it transforms the person who sees it.” -B.K.S
-    Iyengar’,
-    ‘“Happiness does not depend on what you have or who you are. It solely relies on what you
-    think.” -Buddha’,
-    ‘“In the middle of difficulty lies opportunity.” -Albert Einstein’
+    '"The nature of yoga is to shine the light of awareness into the darkest corners of the body." -Jason Crandell',
+    '"Change only happens in the present moment.The past is already done. The future is just energy and intention."-Kino McGregor',
+    '"Yoga begins with listening. When we listen, we are giving space to what is." -Richard Freeman', 
+    '"The body benefits from movement, and the mind benefits from stillness." -Sakyong Mipham',
+    '"Yoga does not transform the way we see things, it transforms the person who sees." -B.K.S Iyengar',
+    '"Happiness does not depend on what you have or who you are. It solely relies on what you think." -Buddha',
+    '"In the middle of difficulty lies opportunity." -Albert Einstein'
+   
 ];
+
+btn.addEventListener('click', function() {
+    let randomQuote = quote[Math.floor(Math.random() * quote.length)]
+    output.innerHTML = randomQuote;
+});
