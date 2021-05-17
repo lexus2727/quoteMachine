99b103ff637b4673185da0c20cad2e73c9a9a5a6
Candice Parker <candiceparker2008@yahoo.com>
2021-05-17 19:21:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ let quote = ['"The nature of yoga is to shine the light of awareness into the da
     '"Yoga does not transform the way we see things, it transforms the person who sees." -B.K.S Iyengar',
     '"Happiness does not depend on what you have or who you are. It solely relies on what you think." -Buddha',
     '"In the middle of difficulty lies opportunity." -Albert Einstein',
-    '"There is no health without mental health; mental health is too important to be left to the professionals alone, and mental health is everyone’s business." — Vikram Patel',
     '“Keeping your body healthy is an expression of gratitude to the whole cosmos- the trees, the clouds, everything.” – Thich Nhat Hanh',
     '“What drains your spirit drains your body. What fuels your spirit fuels your body.” – Caroline Myss',
     '“A calm mind brings inner strength and self-confidence, so that’s very important for good health.” – Dalai Lama',
